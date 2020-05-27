9166d336156d08b15fe07b4338cf73f8ac21d0d0
Rachel Moser <moser.rachel@gmail.com>
2020-05-27 16:56:56 -0500

    game test formatting

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,9 +11,17 @@ describe Game do
   let(:player1) { Player.new('foo', 1) }
   let(:player2) { Player.new('bar', 2) }
   context '#switch_current_player' do
-    it 'returns player 1 if player 2 was previous player' do
-      game = double
-      allow(game).to receive(:switch_current_player).with(player1).and_return(player2)
+    context 'player 2 was the previous player' do
+      it 'should return player1' do
+        game = double
+        allow(game).to receive(:switch_current_player).with(player1).and_return(player2)
+      end
+    end
+    context 'player 1 was the previous player' do
+      it 'should return player2' do
+        game = double
+        allow(game).to receive(:switch_current_player).with(player2).and_return(player1)
+      end
     end
   end
 end
