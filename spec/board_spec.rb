54d47938989b611a095b4e8f41315ae1d747828f
Rachel Moser <moser.rachel@gmail.com>
2020-05-27 16:54:06 -0500

    test formatting

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,19 +4,28 @@
 require_relative '../lib/board'
 require_relative '../lib/display'
 require_relative '../lib/player'
-require_relative './shared_spec'
 
 # rubocop:disable Metrics/BlockLength, Style/WordArray
 describe Board do
-  include_context 'common'
+  # let(:player) { Player.new('test', 1) }
+  let(:player1) { Player.new('test', 1) }
+  subject { described_class.new }
   context '#initialize' do
     it 'creates an empty grid of 6 rows by 7 columns' do
-      expect(example.board).to eql(empty_array)
+      empty_array = [
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '']
+      ]
+      expect(subject.board).to eql(empty_array)
     end
   end
   context '#valid_move?' do
-    before :each do
-      example.board = [
+    before do
+      subject.board = [
         ['2', '2', '', '', '', '', ''],
         ['1', '1', '', '', '', '', ''],
         ['2', '1', '', '', '', '', ''],
@@ -25,16 +34,20 @@ describe Board do
         ['1', '2', '1', '2', '2', '1', '2']
       ]
     end
-    it 'returns truthy if column has empty places' do
-      expect(example.valid_move?(2)).to be_truthy
+    context 'column is empty' do
+      it 'should be true' do
+        expect(subject.valid_move?(2)).to be true
+      end
     end
-    it 'returns falsey if column does not have empty places' do
-      expect(example.valid_move?(1)).to be_falsey
+    context 'column is full' do
+      it 'should be false' do
+        expect(subject.valid_move?(1)).to be false
+      end
     end
   end
   context '#update' do
     before :each do
-      example.board = [
+      subject.board = [
         ['', '', '', '', '', '', ''],
         ['', '', '', '', '', '', ''],
         ['2', '', '', '', '', '', ''],
@@ -43,158 +56,195 @@ describe Board do
         ['1', '2', '', '', '', '', '']
       ]
     end
-    it 'updates bottom row if entire column is empty' do
-      solution = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['2', '', '', '', '', '', ''],
-        ['1', '1', '', '', '', '', ''],
-        ['2', '2', '', '', '', '', ''],
-        ['1', '2', '', '', '1', '', '']
-      ]
-      expect(example.update(4, player)).to eql(solution)
+    context 'column is empty' do
+      it 'should update bottom row' do
+        solution = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['2', '', '', '', '', '', ''],
+          ['1', '1', '', '', '', '', ''],
+          ['2', '2', '', '', '', '', ''],
+          ['1', '2', '', '', '1', '', '']
+        ]
+        expect(subject.update(4, player1)).to eql(solution)
+      end
     end
-    it 'updates lowest empty row if columns has some values' do
-      solution = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['2', '1', '', '', '', '', ''],
-        ['1', '1', '', '', '', '', ''],
-        ['2', '2', '', '', '', '', ''],
-        ['1', '2', '', '', '', '', '']
-      ]
-      expect(example.update(1, player)).to eql(solution)
+    context 'column has a some values' do
+      it 'should update lowest empty row' do
+        solution = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['2', '1', '', '', '', '', ''],
+          ['1', '1', '', '', '', '', ''],
+          ['2', '2', '', '', '', '', ''],
+          ['1', '2', '', '', '', '', '']
+        ]
+        expect(subject.update(1, player1)).to eql(solution)
+      end
     end
   end
   context '#full?' do
-    it 'returns true when board is full' do
-      example.board = [
-        ['2', '2', '1', '2', '2', '2', '1'],
-        ['1', '1', '2', '2', '1', '1', '2'],
-        ['2', '1', '1', '1', '2', '2', '1'],
-        ['1', '2', '2', '2', '1', '1', '2'],
-        ['2', '1', '2', '1', '1', '2', '1'],
-        ['1', '2', '1', '2', '2', '1', '2']
-      ]
-      expect(example.full?).to be_truthy
+    context 'board is full' do
+      it 'should be true' do
+        subject.board = [
+          ['2', '2', '1', '2', '2', '2', '1'],
+          ['1', '1', '2', '2', '1', '1', '2'],
+          ['2', '1', '1', '1', '2', '2', '1'],
+          ['1', '2', '2', '2', '1', '1', '2'],
+          ['2', '1', '2', '1', '1', '2', '1'],
+          ['1', '2', '1', '2', '2', '1', '2']
+        ]
+        expect(subject.full?).to be true
+      end
     end
-    it 'returns false when board is not full' do
-      example.board = [
-        ['2', '2', '1', '2', '', '', ''],
-        ['1', '1', '2', '2', '', '1', '2'],
-        ['2', '1', '1', '1', '2', '2', '1'],
-        ['1', '2', '2', '2', '1', '1', '2'],
-        ['2', '1', '2', '1', '1', '2', '1'],
-        ['1', '2', '1', '2', '2', '1', '2']
-      ]
-      expect(example.full?).to be_falsey
+    context 'board is not full' do
+      it 'should be false' do
+        subject.board = [
+          ['2', '2', '1', '2', '', '', ''],
+          ['1', '1', '2', '2', '', '1', '2'],
+          ['2', '1', '1', '1', '2', '2', '1'],
+          ['1', '2', '2', '2', '1', '1', '2'],
+          ['2', '1', '2', '1', '1', '2', '1'],
+          ['1', '2', '1', '2', '2', '1', '2']
+        ]
+        expect(subject.full?).to be false
+      end
     end
   end
   context '#row_victory?' do
-    it 'returns true when a row has 4 in a row' do
-      example.board = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['1', '1', '1', '1', '', '', ''],
-        ['1', '1', '1', '2', '', '', ''],
-        ['2', '2', '1', '2', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.row_victory?).to be_truthy
+    context 'row has four-in-a-row' do
+      it 'should be true' do
+        subject.board = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['1', '1', '1', '1', '', '', ''],
+          ['1', '1', '1', '2', '', '', ''],
+          ['2', '2', '1', '2', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.row_victory?).to be true
+      end
     end
-    it 'returns false when no rows has 4 in a row' do
-      example.board = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['2', '1', '', '', '', '', ''],
-        ['1', '2', '1', '1', '', '', ''],
-        ['2', '1', '2', '2', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.row_victory?).to be_falsey
+    context 'row does not have four-in-a-row' do
+      it 'should be false' do
+        subject.board = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['2', '1', '', '', '', '', ''],
+          ['1', '2', '1', '1', '', '', ''],
+          ['2', '1', '2', '2', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.row_victory?).to be false
+      end
     end
   end
   context '#column_victory?' do
-    it 'returns true when a column has 4 in a row' do
-      example.board = [
-        ['1', '', '', '', '', '', ''],
-        ['1', '', '', '', '', '', ''],
-        ['1', '1', '1', '1', '', '', ''],
-        ['1', '1', '1', '2', '', '', ''],
-        ['2', '2', '1', '2', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.column_victory?).to be_truthy
+    context 'column has four-in-a-row' do
+      it 'should be true' do
+        subject.board = [
+          ['1', '', '', '', '', '', ''],
+          ['1', '', '', '', '', '', ''],
+          ['1', '1', '1', '1', '', '', ''],
+          ['1', '1', '1', '2', '', '', ''],
+          ['2', '2', '1', '2', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.column_victory?).to be true
+      end
     end
-    it 'returns false when no columns has 4 in a row' do
-      example.board = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['2', '1', '', '', '', '', ''],
-        ['1', '2', '1', '1', '', '', ''],
-        ['2', '1', '2', '2', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.column_victory?).to be_falsey
+    context 'column does not have four-in-a-row' do
+      it 'should be false' do
+        subject.board = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['2', '1', '', '', '', '', ''],
+          ['1', '2', '1', '1', '', '', ''],
+          ['2', '1', '2', '2', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.column_victory?).to be false
+      end
     end
   end
   context '#diagonal_victory?' do
-    it 'returns true when there is 4 in a forward diagonal row' do
-      example.board = [
-        ['1', '', '', '', '', '', ''],
-        ['1', '', '', '', '', '', ''],
-        ['1', '1', '1', '1', '', '', ''],
-        ['1', '1', '1', '2', '', '', ''],
-        ['2', '2', '1', '1', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.diagonal_victory?).to be_truthy
+    context 'there is a diagonal four-in-a-row' do
+      it 'should be true' do
+        subject.board = [
+          ['1', '', '', '', '', '', ''],
+          ['1', '', '', '', '', '', ''],
+          ['1', '1', '1', '1', '', '', ''],
+          ['1', '1', '1', '2', '', '', ''],
+          ['2', '2', '1', '1', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.diagonal_victory?).to be true
+      end
+      it 'should be true' do
+        subject.board = [
+          ['1', '', '', '', '', '', ''],
+          ['2', '', '', '', '', '', ''],
+          ['1', '1', '1', '1', '', '', ''],
+          ['1', '2', '1', '2', '', '', ''],
+          ['2', '1', '1', '1', '', '', ''],
+          ['1', '2', '2', '2', '', '', '']
+        ]
+        expect(subject.diagonal_victory?).to be true
+      end
     end
-    it 'returns true when there is 4 in a backward diagonal row' do
-      example.board = [
-        ['1', '', '', '', '', '', ''],
-        ['2', '', '', '', '', '', ''],
-        ['1', '1', '1', '1', '', '', ''],
-        ['1', '2', '1', '2', '', '', ''],
-        ['2', '1', '1', '1', '', '', ''],
-        ['1', '2', '2', '2', '', '', '']
-      ]
-      expect(example.diagonal_victory?).to be_truthy
-    end
-    it 'returns false when there is not 4 in a diagonal row' do
-      example.board = [
-        ['', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', ''],
-        ['2', '1', '', '', '', '', ''],
-        ['1', '2', '1', '1', '', '', ''],
-        ['2', '1', '2', '2', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.diagonal_victory?).to be_falsey
+    context 'there is not a diagonal four-in-a-row' do
+      it 'should be false' do
+        subject.board = [
+          ['', '', '', '', '', '', ''],
+          ['', '', '', '', '', '', ''],
+          ['2', '1', '', '', '', '', ''],
+          ['1', '2', '1', '1', '', '', ''],
+          ['2', '1', '2', '2', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.diagonal_victory?).to be false
+      end
     end
   end
   context '#complete?' do
-    it 'returns true when the board is full or four-in-a-row' do
-      example.board = [
-        ['1', '', '', '', '', '', ''],
-        ['1', '', '', '', '', '', ''],
-        ['1', '1', '1', '1', '', '', ''],
-        ['1', '1', '1', '2', '', '', ''],
-        ['2', '2', '1', '1', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.complete?).to be_truthy
+    context 'board has four-in-a-row' do
+      it 'should be true' do
+        subject.board = [
+          ['1', '', '', '', '', '', ''],
+          ['1', '', '', '', '', '', ''],
+          ['1', '1', '1', '1', '', '', ''],
+          ['1', '1', '1', '2', '', '', ''],
+          ['2', '2', '1', '1', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.complete?).to be true
+      end
     end
-    it 'returns false when the board is not full or four-in-a-row' do
-      example.board = [
-        ['1', '', '', '', '', '', ''],
-        ['2', '', '', '', '', '', ''],
-        ['1', '1', '2', '1', '', '', ''],
-        ['1', '2', '2', '2', '', '', ''],
-        ['2', '2', '1', '1', '', '', ''],
-        ['1', '2', '2', '1', '', '', '']
-      ]
-      expect(example.complete?).to be_falsey
+    context 'board is full' do
+      it 'should be true' do
+        subject.board = [
+          ['2', '2', '1', '2', '2', '2', '1'],
+          ['1', '1', '2', '2', '1', '1', '2'],
+          ['2', '1', '1', '1', '2', '2', '1'],
+          ['1', '2', '2', '2', '1', '1', '2'],
+          ['2', '1', '2', '1', '1', '2', '1'],
+          ['1', '2', '1', '2', '2', '1', '2']
+        ]
+        expect(subject.complete?).to be true
+      end
+    end
+    context 'board is not full or have four-in-a-row' do
+      it 'should be false' do
+        subject.board = [
+          ['1', '', '', '', '', '', ''],
+          ['2', '', '', '', '', '', ''],
+          ['1', '1', '2', '1', '', '', ''],
+          ['1', '2', '2', '2', '', '', ''],
+          ['2', '2', '1', '1', '', '', ''],
+          ['1', '2', '2', '1', '', '', '']
+        ]
+        expect(subject.complete?).to be false
+      end
     end
   end
 end
