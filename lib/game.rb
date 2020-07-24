327273edc6486db01865363a9c2ddb066f17583a
Rachel Moser <moser.rachel@gmail.com>
2020-07-24 15:44:25 -0500

    update_board test

--- lib/game.rb
+++ lib/game.rb
@@ -22,17 +22,33 @@ class Game
     play_game
   end
 
+  def create_player(number)
+    puts display_name(number)
+    name = gets.chomp
+    Player.new(name, number)
+  end
+
+  def update_board(column, player)
+    board.update(column, player)
+    board.display_game
+  end
+
+  def repeat_game
+    puts display_play_again(first_player, second_player)
+    repeat = gets.chomp
+    return unless repeat == 'y'
+
+    @board = GameBoard.new
+    play_game
+  end
+
+  protected
+
   def play_game
     board.display_game
     @current_player = first_player
     turn_order
     game_over
-  end
-
-  def create_player(number)
-    puts display_name(number)
-    name = gets.chomp
-    Player.new(name, number)
   end
 
   def turn_order
@@ -47,42 +63,15 @@ class Game
     end
   end
 
+  def player_turn_input(player)
+    verify_input(player, player_input(turn_prompt(player)))
+  end
+
   def verify_input(player, input)
     return input if input.downcase == 'exit'
     return input if board.valid_move?(input.to_i - 1)
 
     puts display_column_full
-    verify_input(player, player_input(turn_prompt(player)))
-  end
-
-  def update_board(column, player)
-    board.update(column, player)
-    board.display_game
-  end
-
-  def game_over
-    if board.full?
-      puts display_draw
-    elsif board.complete?
-      puts display_winner(@current_player)
-    else
-      puts display_exit
-    end
-    repeat_game
-  end
-
-  def repeat_game
-    puts display_play_again(first_player, second_player)
-    repeat = gets.chomp
-    return unless repeat == 'y'
-
-    @board = GameBoard.new
-    play_game
-  end
-
-  protected
-
-  def player_turn_input(player)
     verify_input(player, player_input(turn_prompt(player)))
   end
 
@@ -104,5 +93,16 @@ class Game
 
   def switch_current_player
     @current_player = @current_player == first_player ? second_player : first_player
+  end
+
+  def game_over
+    if board.full?
+      puts display_draw
+    elsif board.complete?
+      puts display_winner(@current_player)
+    else
+      puts display_exit
+    end
+    repeat_game
   end
 end
